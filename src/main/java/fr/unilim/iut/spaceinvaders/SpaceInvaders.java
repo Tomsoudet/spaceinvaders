97f9616654c7719694be6c96e91d28c9b4dd85e5
Tom <tsoudet@hotmail.fr>
2020-04-07 15:48:11 +0200

    positionner un nouveau vaisseau (f1 s2)

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,25 +1,64 @@
- package fr.unilim.iut.spaceinvaders;
-  
-    public class SpaceInvaders {
+package fr.unilim.iut.spaceinvaders;
 
-	    int longueur;
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
+public class SpaceInvaders {
+
+	    private static final char MARQUE_FIN_LIGNE = '\n';
+		private static final char MARQUE_VIDE = '.';
+		private static final char MARQUE_VAISSEAU = 'V';
+		int longueur;
 	    int hauteur;
+	    Vaisseau vaisseau;
 
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
 		   this.hauteur = hauteur;
 	   }
 	    
-	    @Override
-		public String toString() {
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+			if (  !estDansEspaceJeu(x, y) )
+				throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
+			vaisseau = new Vaisseau(x, y); 
+		}
+
+		private boolean estDansEspaceJeu(int x, int y) {
+			return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
+		}
+	    
+	    private boolean aUnVaisseau() {
+			return vaisseau!=null;
+		}
+	    
+	    private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+			return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+		}
+	    
+	    private char recupererMarqueDeLaPosition(int x, int y) {
+			char marque;
+			if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+			      marque=MARQUE_VAISSEAU;
+			else
+			      marque=MARQUE_VIDE;
+			return marque;
+		}
+
+	    public String recupererEspaceJeuDansChaineASCII() {
 			StringBuilder espaceDeJeu = new StringBuilder();
-			for (int i = 0; i < hauteur; i++) {
-				for (int j = 0; j < longueur; j++) {
-					espaceDeJeu.append('.');
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					 espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 				}
-				espaceDeJeu.append('\n');
+				espaceDeJeu.append(MARQUE_FIN_LIGNE);
 			}
 			return espaceDeJeu.toString();
 		}
+	    
+	    @Override
+		public String toString() {
+			return recupererEspaceJeuDansChaineASCII();
+		}
+
+	
 
    }
